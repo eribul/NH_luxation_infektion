a5649aeae88fae14a3615c723b6dc82103b3923c
Erik Bülow <eriklgb@gmail.com>
2020-05-11 12:36:30 +0200

    add baseline variables to coef table. Fix #22.

--- src/tab_coefs.R
+++ src/tab_coefs.R
@@ -2,6 +2,19 @@ suppressMessages({library(ProjectTemplate); load.project()})
 
 # Table to present --------------------------------------------------------
 
+baseline <-
+  crossing(
+    time = factor(c("90 days", "2 years")),
+    term = c(
+      paste("Sex:", levels(df$P_Sex)[1]),
+      paste("BMI:", levels(df$P_BMI)[1]),
+      paste("Diagnosis:", levels(df$P_DiaGrp)[1])
+    )
+  ) %>%
+  mutate(beta = 0, `OR 95 % CI` = "(baseline)", p = "")
+
+
+
 tbl_coefs <-
   infection_data %>%
   unnest("all_models") %>%
@@ -12,12 +25,22 @@ tbl_coefs <-
   unnest(tidy) %>%
   transmute(
     time = factor(time, c("90d", "2y"), c("90 days", "2 years")),
+    term = gsub("Male sex", "Sex: Male", term),
     term = clean_names(term),
     beta = log(estimate),
     `OR 95 % CI` = sprintf("%.2f (%.2f-%.2f)", estimate, conf.low, conf.high),
     p    = pvalue_format(.01)(p.value)
   ) %>%
-  arrange(time) %>%
-  mutate_at(vars(`OR 95 % CI`, p), ~ if_else(term == "(Intercept)", "", .))
+  bind_rows(baseline) %>%
+  separate(term, c("variable", "level"), sep = ": ") %>%
+  arrange(time, variable, `OR 95 % CI`) %>%
+  mutate_at(vars(`OR 95 % CI`, p), ~ if_else(variable == "(Intercept)", "", .)) %>%
+  group_by(time) %>%
+  mutate(variable = replace(variable, duplicated(variable), "")) %>%
+  ungroup() %>%
+  mutate(
+    time = as.character(time),
+    time = replace(time, duplicated(time), "")
+  )
 
 cache("tbl_coefs")
