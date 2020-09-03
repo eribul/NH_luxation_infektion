6f97b3a5bc26898202815aefada552c7827da022
Erik Bülow <eriklgb@gmail.com>
2020-09-03 06:56:06 +0200

    include validation template

---
+++ src/export_for_validation.R
@@ -0,0 +1,24 @@
+suppressMessages({library(ProjectTemplate); load.project()})
+
+# Hitta alla koefficientnamn som krävs för både 90 dagar och 2 år
+model_vars <-
+  fit_brl %>%
+  mutate(
+    vars = map(fit, ~names(coef(.)))
+  ) %>%
+  select(vars) %>%
+  pluck(1) %>%
+  c(recursive = TRUE) %>%
+  unique() %>%
+  {.[-1]} %>%
+  {gsub("TRUE|I{2,3}|Male", "", .)} %>%
+  {gsub("(P_BMI)(.*)", "\\1", ., perl = TRUE)} %>%
+  {gsub("(P_DiaGrp)(.*)", "\\1", ., perl = TRUE)} %>%
+  unique()
+
+# Export data frame of data needed for external validation
+ext_val_required_data <-
+  df %>%
+  select(one_of(model_vars))
+
+cache("ext_val_required_data")
